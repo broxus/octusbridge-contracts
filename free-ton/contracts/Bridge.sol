2b50f5889da2abb89021e3b4c3134ff3e8ef1631
aleksandr-hramcov <aleksandr.hramcov@gmail.com>
2020-11-11 00:24:47 +0300

    add TonEventConfiguration

--- free-ton/contracts/Bridge.sol
+++ free-ton/contracts/Bridge.sol
@@ -85,4 +85,131 @@ contract FreeTonBridge {
         tvm.accept();
         return ethereumEventsConfiguration;
     }
+
+    //tonToEth
+
+    mapping(address => bool) currentRelays;
+    uint8 currentRelaysCount;
+
+    //change nonce => is applied
+    mapping(uint256 => bool) appliedChanges;
+    uint8 eventConfigurationRequiredConfirmationsPercent = 65;
+
+    mapping(address => uint256) relaysTonPublicKeys;
+//  mapping(address => uint256) relaysEthPublicKeys;
+
+    //TODO: merge it with EthereumEventConfiguration to EventConfiguration
+    struct TonEventConfiguration {
+        address tonAddress;
+        bytes ethAddress;
+//      bytes ethEventABI;
+//      address eventProxyAddress;
+        uint8 minSigns;
+        uint8 minSignsPercent;
+    }
+
+//  mapping(bytes => address) ethToTonMap;
+    mapping(address => bytes) tonToEthMap;
+    mapping(address => TonEventConfiguration) tonToConfig;
+
+    function validSignaturesCount(
+        uint256 hash,
+        address[] signers,
+        uint256[] signaturesHighParts,
+        uint256[] signaturesLowParts) private returns (uint8) {
+
+        require(signers.length == signaturesHighParts.length);
+        require(signers.length == signaturesLowParts.length);
+
+        uint8 validCount = 0;
+
+        for (uint i = 0; i < signers.length; i++){
+            if (currentRelays.exists(signers[i]) && currentRelays.at(signers[i]) && relaysTonPublicKeys.exists(signers[i]) &&
+                tvm.checkSign(hash, signaturesHighParts[i], signaturesLowParts[i], relaysTonPublicKeys.at(signers[i]))) {
+                validCount++;
+            }
+        }
+
+        return validCount;
+    }
+
+    function addEventType(
+        address tonAddress,
+        bytes ethAddress,
+//      bytes ethEventABI,
+//      address eventProxyAddress,
+        uint8 minSigns,
+        uint8 minSignsPercent,
+        uint256 changeNonce,
+        address[] signers,
+        uint256[] signaturesHighParts,
+        uint256[] signaturesLowParts) public {
+
+        require(!appliedChanges.exists(changeNonce));
+        require(!tonToEthMap.exists(tonAddress));
+//      require(!ethToTonMap.exists(ethAddress));
+
+        TvmBuilder builder;
+        builder.store(
+            tonAddress,
+            ethAddress,
+//          ethEventABI
+//          eventProxyAddress,
+            minSigns,
+            minSignsPercent,
+            changeNonce);
+        TvmCell cell = builder.toCell();
+        uint256 hash = tvm.hash(cell);
+
+        if (validSignaturesCount(hash, signers, signaturesHighParts, signaturesLowParts) * 100 / currentRelaysCount >=
+            eventConfigurationRequiredConfirmationsPercent) {
+
+            appliedChanges[changeNonce] = true;
+
+            TonEventConfiguration newConfig = TonEventConfiguration({
+                tonAddress: tonAddress,
+                ethAddress: ethAddress,
+//              ethEventABI: ethEventABI,
+//              eventProxyAddress: eventProxyAddress,
+                minSigns: minSigns,
+                minSignsPercent: minSignsPercent
+            });
+
+//          ethToTonMap[ethAddress] = tonAddress;
+            tonToEthMap[tonAddress] = ethAddress;
+            tonToConfig[tonAddress] = newConfig;
+
+        }
+
+    }
+
+    function removeEventType(
+        address tonAddress,
+        uint256 changeNonce,
+        address[] signers,
+        uint256[] signaturesHighParts,
+        uint256[] signaturesLowParts) public {
+
+        require(!appliedChanges.exists(changeNonce));
+
+        TvmBuilder builder;
+        builder.store(tonAddress, changeNonce);
+        TvmCell cell = builder.toCell();
+        uint256 hash = tvm.hash(cell);
+
+        //FIXME: change required signatures count
+        if (validSignaturesCount(hash, signers, signaturesHighParts, signaturesLowParts) * 100 / currentRelaysCount >=
+            eventConfigurationRequiredConfirmationsPercent) {
+
+            appliedChanges[changeNonce] = true;
+//          bytes ethAddress = tonToEthMap.at(tonAddress);
+//          delete ethToTonMap[ethAddress];
+            delete tonToEthMap[tonAddress];
+            delete tonToConfig[tonAddress];
+
+        }
+
+    }
+
+
 }
