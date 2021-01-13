6c6acfba5b44617d95d096ac4bb43246ec1c98cb
Sergey P <pavlovdog.moscow@gmail.com>
2021-01-13 09:34:14 +0300

    Update migration

--- ethereum/migrations/2_deploy_bridge.js
+++ ethereum/migrations/2_deploy_bridge.js
@@ -2,7 +2,6 @@ require('dotenv').config({ path: './../../env/ethereum.env' });
 
 const Bridge = artifacts.require("Bridge");
 const ProxySimple = artifacts.require("ProxySimple");
-const ProxyToken = artifacts.require("ProxyToken");
 
 
 module.exports = async (deployer, network ,accounts) => {
@@ -13,10 +12,6 @@ module.exports = async (deployer, network ,accounts) => {
   
   await deployer.deploy(
     ProxySimple,
-  );
-  
-  await deployer.deploy(
-    ProxyToken,
     Bridge.address,
-  )
+  );
 };
