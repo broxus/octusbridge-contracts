4c3b6d3bd1dbd19650683e58ce081f50cc44e208
Sergey P <pavlovdog.moscow@gmail.com>
2021-01-21 14:55:50 +0300

    Update Ethereum TON event structure

--- ethereum/contracts/interfaces/IProxy.sol
+++ ethereum/contracts/interfaces/IProxy.sol
@@ -5,10 +5,9 @@ pragma experimental ABIEncoderV2;
 interface IProxy {
     struct TONEvent {
         uint eventTransaction;
-        uint eventIndex;
+        uint64 eventTransactionLt;
+        uint32 eventIndex;
         bytes eventData;
-        uint eventBlockNumber;
-        uint eventBlock;
         bytes tonEventConfiguration;
         uint requiredConfirmations;
         uint requiredRejects;
