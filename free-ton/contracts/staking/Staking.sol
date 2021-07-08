62b78b105b7d899cc1bdc17b7a71b2e49a8e33b0
Maga <steam.30mb1@gmail.com>
2021-07-08 20:44:51 +0300

    Updated constructor

--- free-ton/contracts/staking/Staking.sol
+++ free-ton/contracts/staking/Staking.sol
@@ -3,10 +3,11 @@ pragma ton-solidity ^0.39.0;
 import "./base/StakingRelay.sol";
 
 contract Staking is StakingPoolRelay {
-    constructor(address _admin, address _dao_root, address _rewarder, address _tokenRoot) public {
+    constructor(address _admin, address _dao_root, address _rewarder, address _bridge, address _tokenRoot) public {
         tvm.accept();
 
         tokenRoot = _tokenRoot;
+        bridge = _bridge;
         admin = _admin;
         dao_root = _dao_root;
         rewarder = _rewarder;
