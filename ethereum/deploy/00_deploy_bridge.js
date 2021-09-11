4173a221185ee9d78c4d0bff4d2f5d88dd10efbd
Sergey P <pavlovdog.moscow@gmail.com>
2021-09-11 13:26:35 +0300

    Update Ethereum side
    - Fixed Apache license

--- ethereum/deploy/00_deploy_bridge.js
+++ ethereum/deploy/00_deploy_bridge.js
@@ -1,7 +1,11 @@
 module.exports = async ({getNamedAccounts, deployments}) => {
   const { deployer, owner } = await getNamedAccounts();
 
+  console.log(deployer, owner);
+  
   const relays = await ethers.getSigners();
+  
+  console.log(relays.map(a => a.address));
   
   await deployments.deploy('Bridge', {
     from: deployer,
@@ -12,11 +16,15 @@ module.exports = async ({getNamedAccounts, deployments}) => {
         methodName: 'initialize',
         args: [
           owner,
-          5, // Minimum required signatures
-          100, // Relay TTL after round in seconds
+          3, // Minimum required signatures
+          604800, // Relay TTL after round in seconds, 1 week
           0, // Initial round number
           Math.floor(Date.now() / 1000) + 604800, // Initial round end, after 1 week
-          relays.map(a => a.address), // Initial relays
+          [
+            "0x59861a7db8e01daf3763468325161e41bec59821",
+            "0x440734bbacc1cfae9b5b16f14eb7423a1f069af0",
+            "0x0fa6339155d9dd1fa7e4fd8feba84c675b5874ff"
+          ], // Initial relays
         ],
       }
     }
