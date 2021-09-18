d2e4b4402e634db652af5eaeed12ca399e4c2553
Sergey P <pavlovdog.moscow@gmail.com>
2021-09-18 22:54:33 +0300

    Add template for movement fees test

---
+++ ethereum/test/vault-dai/3-movement-fee.js
@@ -0,0 +1,21 @@
+describe('Check deposit / withdraw fees', async () => {
+  it('Setup contracts', async () => {
+  
+  });
+  
+  it('Set non-zero deposit fee', async () => {
+  
+  });
+
+  it('Set non-zero withdraw fee', async () => {
+  
+  });
+
+  it('Check deposit fee is considered', async () => {
+  
+  });
+  
+  it('Check withdraw fee is considered', async () => {
+  
+  });
+});
