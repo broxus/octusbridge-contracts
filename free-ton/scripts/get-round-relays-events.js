1d482d77820f0c18e0dc5f3d1d7f2d8f8d2f615f
pavlovdog <pavlovdog.moscow@gmail.com>
2021-11-11 14:02:18 +0300

    Update round uploader script

--- free-ton/scripts/get-round-relays-events.js
+++ free-ton/scripts/get-round-relays-events.js
@@ -3,85 +3,95 @@ const {
 } = require('./../test/utils');
 
 const ethers = require('ethers');
-const prompts = require('prompts');
 const fs = require('fs');
 
 
+const requireEnv = (name, _default) => {
+  const value = process.env[name];
+
+  if (value === undefined && _default === undefined) {
+    throw new Error(`Missing env at ${name}`);
+  }
+
+  return value || _default;
+};
+
+
+const getEvents = async (configuration, ge_created_at=0) => {
+  const {
+    result
+  } = (await locklift.ton.client.net.query_collection({
+        collection: 'messages',
+        filter: {
+          src: {
+            eq: configuration.address
+          },
+          msg_type: {
+            eq: 2
+          },
+          created_at: {
+            gt: ge_created_at
+          }
+        },
+        result: 'body id src created_at',
+      }
+  ));
+
+  const events = (await configuration.decodeMessages(result, true, 'output'))
+      .filter(m => m.name === 'NewEventContract');
+
+  if (events.length === 0) return [];
+
+  return [
+    ...events,
+    ...(await getEvents(configuration, events[events.length - 1].created_at))
+  ];
+};
+
+
+
 const main = async () => {
-  const responses = await prompts([
-    {
-      type: 'select',
-      name: 'rpc',
-      message: 'Choose Ethereum network',
-      choices: [
-        { title: 'Goerli',  value: process.env.ETH_GOERLI_HTTP },
-        { title: 'Ethereum',  value: process.env.ETH_MAIN_HTTP },
-        { title: 'Ropsten', value: process.env.ETH_ROPSTEN_HTTP }
-      ],
-    },
-    {
-      type: 'text',
-      name: 'eth_bridge',
-      message: 'Ethereum bridge contract',
-      validate: value => ethers.utils.isAddress(value) ? true : 'Invalid Ethereum address',
-      initial: '0x5B152cCf3daAF4bb5083Aa7B1A5e5a6b81D400a0'
-    },
-    {
-      type: 'text',
-      name: 'round_relays_configuration',
-      message: 'TON round & relays configuration',
-      validate: value => isValidTonAddress(value) ? true : 'Invalid TON address',
-      initial: '0:dcb0e44ed53e2c4395e38acd752a8e602a8bd004797fa05fa764c03989ae6f21'
-    },
-    {
-      type: 'text',
-      name: 'round_submitter_seed',
-      message: 'Round Submitter Seed Phrase',
-    },
-    {
-      type: 'text',
-      name: 'cell_encoder',
-      message: 'TON CellEncoderStandalone',
-      validate: value => isValidTonAddress(value) ? true : 'Invalid TON address',
-      initial: '0:ff290020c10a2813107c50ba45a7859c67b2d240fe4ab4f34f9954a75aded5c3'
-    },
-  ]);
+  const rpc = requireEnv('EVM_RPC');
+  const bridgeAddress = requireEnv('EVM_BRIDGE');
+  const configuration = requireEnv('ROUND_RELAYS_CONFIGURATION');
+  const seed = requireEnv('EVM_SEED');
+  const cellEncoder = requireEnv('CELL_ENCODER');
+  const targetGasPrice = ethers.utils.parseUnits(requireEnv('TARGET_GAS_PRICE'), 9);
 
   // Connect to the Ethereum
-  const provider = new ethers.providers.JsonRpcProvider(responses.rpc);
+  const provider = new ethers.providers.JsonRpcProvider(rpc);
   const bridge = new ethers.Contract(
-    responses.eth_bridge,
+    bridgeAddress,
     JSON.parse(fs.readFileSync('./../ethereum/abi/Bridge.json')),
     provider
   );
-  const submitter = ethers.Wallet.fromMnemonic(responses.round_submitter_seed).connect(provider);
+  const submitter = ethers.Wallet.fromMnemonic(seed).connect(provider);
 
   const lastRound = await bridge.lastRound();
-  
+
   console.log(`Last round in Ethereum bridge: ${lastRound}`);
 
   // Get events from the configuration
   const roundRelaysConfiguration = await locklift.factory.getContract('TonEventConfiguration');
-  roundRelaysConfiguration.address = responses.round_relays_configuration;
+  roundRelaysConfiguration.address = configuration;
 
   const cellEncoderStandalone = await locklift.factory.getContract('CellEncoderStandalone');
-  cellEncoderStandalone.setAddress(responses.cell_encoder);
+  cellEncoderStandalone.setAddress(cellEncoder);
 
-  const events = await roundRelaysConfiguration.getEvents('NewEventContract');
-  
+  // const events = await roundRelaysConfiguration.getEvents('NewEventContract');
+  const events = await getEvents(roundRelaysConfiguration);
+
+  const roundRelaysConfigurationDetails = await roundRelaysConfiguration.call({ method: 'getDetails' });
+
   console.log(`Found ${events.length} events`);
-  // console.log(new Date(events[events.length - 1].created_at * 1000));
-  // const minutesSinceLastEvent = (new Date() - new Date(events[events.length - 1].created_at * 1000));
-  //
-  // console.log(new Date(minutesSinceLastEvent * 1000).getMinutes());
-  
+
   // Get event details
   const eventDetails = await Promise.all(events.map(async (event) => {
     const stakingTonEvent = await locklift.factory.getContract('StakingTonEvent');
     stakingTonEvent.address = event.value.eventContract;
 
     const details = await stakingTonEvent.call({method: 'getDetails'});
-    
+
     // console.log(stakingTonEvent.address);
     // console.log(details);
 
@@ -117,7 +127,7 @@ const main = async () => {
         configurationAddress: '0x' + roundRelaysConfiguration.address.split(':')[1],
         eventContractWid: event.value.eventContract.split(':')[0],
         eventContractAddress: '0x' + event.value.eventContract.split(':')[1],
-        proxy: responses.eth_bridge,
+        proxy: `0x${roundRelaysConfigurationDetails._networkConfiguration.proxy.toString(16)}`,
         round: roundNumber.toString(),
       }]
     );
@@ -138,32 +148,43 @@ const main = async () => {
       ...details,
       roundNumber,
       encodedEvent,
+      eventData,
+      eventContract: event.value.eventContract,
       signatures: signatures.map((d) => d.sign),
       created_at: event.created_at
     };
 
   }));
-  let gas_limit;
-  for (let event of eventDetails) {
-    console.log(`Round Number: ${event.roundNumber}`);
+
+  for (let event of eventDetails.sort((a,b) => (a.roundNumber < b.roundNumber) ? -1 : 1)) {
+    console.log(`Round Number: ${event.eventData.round_num}`);
+    console.log(`Event contract: ${event.eventContract}`);
     console.log(`Payload: ${event.encodedEvent}`);
     console.log(`Signatures: \n[${event.signatures.map((b) => '0x' + b.toString('hex')).join(',')}]`);
+
     if (event.roundNumber >= lastRound) {
-      console.log(`Submitting next round: ${event.roundNumber}`)
+      console.log(`Submitting round`);
+
+      const gasPrice = await provider.getGasPrice();
+      console.log(`Gas price: ${ethers.utils.formatUnits(gasPrice, "gwei")}`);
+      console.log(`Target gas price: ${ethers.utils.formatUnits(targetGasPrice, "gwei")}`);
+
+      if (gasPrice.gt(targetGasPrice)) {
+        console.log(`Gas price is too high`);
+        process.exit(1);
+      }
 
       const tx = await bridge.connect(submitter).setRoundRelays(
-        event.encodedEvent, event.signatures, {gasLimit: gas_limit}
+        event.encodedEvent, event.signatures, { gasPrice }
       );
-      if (!gas_limit) {
-        gas_limit = tx.gasLimit;
-      }
+
       console.log(`Transaction: ${tx.hash}`);
 
+      process.exit(0);
     }
+
     console.log('');
   }
-  // Filter out rounds less than Ethereum's last round
-  // Prepare Ethereum's (payload, signatures) for each round
 };
 
 
