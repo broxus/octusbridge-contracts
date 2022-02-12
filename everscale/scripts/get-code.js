b9fee721830c7ce5f7d7be90d95435f737ea7df5
pavlovdog <pavlovdog.moscow@gmail.com>
2022-02-12 22:54:10 +0300

    Update get code script

--- everscale/scripts/get-code.js
+++ everscale/scripts/get-code.js
@@ -1,24 +1,33 @@
 async function main() {
     const TokenTransferEthereumEvent = await locklift.factory.getContract('TokenTransferEthereumEvent');
     console.log('TokenTransferEthereumEvent');
+    console.log('');
     console.log(TokenTransferEthereumEvent.code);
+    console.log('');
 
     const TokenTransferEverscaleEvent = await locklift.factory.getContract('TokenTransferEverscaleEvent');
     console.log('TokenTransferEverscaleEvent');
+    console.log('');
     console.log(TokenTransferEverscaleEvent.code);
+    console.log('');
 
-    // const DaoEthereumActionEvent = await locklift.factory.getContract('DaoEthereumActionEvent');
-    // const StakingEthEvent = await locklift.factory.getContract('StakingEthEvent');
-    // const StakingTonEvent = await locklift.factory.getContract('StakingTonEvent');
-    //
-    // console.log('DaoEthereumActionEvent');
-    // console.log(DaoEthereumActionEvent.code);
-    //
-    // console.log('StakingEthEvent');
-    // console.log(StakingEthEvent.code);
-    //
-    // console.log('StakingTonEvent');
-    // console.log(StakingTonEvent.code);
+    const DaoEthereumActionEvent = await locklift.factory.getContract('DaoEthereumActionEvent');
+    console.log('DaoEthereumActionEvent');
+    console.log('');
+    console.log(DaoEthereumActionEvent.code);
+    console.log('');
+
+    const StakingEthEvent = await locklift.factory.getContract('StakingEthEvent');
+    console.log('StakingEthEvent');
+    console.log('');
+    console.log(StakingEthEvent.code);
+    console.log('');
+
+    const StakingTonEvent = await locklift.factory.getContract('StakingTonEvent');
+    console.log('StakingTonEvent');
+    console.log('');
+    console.log(StakingTonEvent.code);
+    console.log('');
 }
 
 
