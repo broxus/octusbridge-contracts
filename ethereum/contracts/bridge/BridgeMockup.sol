82adb1e701071393095c147a4d77812ea90a97ee
Maga <steam.30mb1@gmail.com>
2022-03-09 19:53:08 +0300

    Removed warnings

--- ethereum/contracts/bridge/BridgeMockup.sol
+++ ethereum/contracts/bridge/BridgeMockup.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.2;
 
 contract BridgeMockup {
     function verifySignedEverscaleEvent(
-        bytes memory payload,
-        bytes[] memory signatures
-    ) external view returns (uint32) {
+        bytes memory /*payload*/,
+        bytes[] memory /*signatures*/
+    ) external pure returns (uint32) {
         return 0;
     }
 }
