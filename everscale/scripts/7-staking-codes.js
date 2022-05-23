a2032f6fd9a835487c79308a2ec37c8f8332fc17
Maga <steam.30mb1@gmail.com>
2022-05-23 15:23:12 +0300

    Fixed script

--- everscale/scripts/7-staking-codes.js
+++ everscale/scripts/7-staking-codes.js
@@ -1,7 +1,7 @@
 async function main() {
-    const StakingV1_1 = await locklift.factory.getContract('StakingV1_2');
+    const StakingV1_2 = await locklift.factory.getContract('StakingV1_2');
     console.log('\nStakingV1_2:\n');
-    console.log(StakingV1_1.code, '\n');
+    console.log(StakingV1_2.code, '\n');
 
     const NewRelayRound = await locklift.factory.getContract('RelayRound');
     console.log('RelayRound:\n');
