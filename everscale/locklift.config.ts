9ebc5e354a76251086bbeef18ed29970e5f204ad
pavlovdog <pavlovdog.moscow@gmail.com>
2023-04-18 16:02:28 +0400

    Update TVM part
    - Remove unused fields from EventCloser
    - Update locklift config: remove giver settings, setup venom / main keys
    - Add script for deploying hidden bridge contracts
    - Update dependencies
    - TODO: fix EVM dependencies

--- everscale/locklift.config.ts
+++ everscale/locklift.config.ts
@@ -1,6 +1,5 @@
 import {LockliftConfig} from "locklift";
 import { FactorySource } from "./build/factorySource";
-import { SimpleGiver, GiverWallet, TestnetGiver } from "./giverSettings";
 const LOCAL_NETWORK_ENDPOINT = "http://localhost/graphql";
 
 declare global {
@@ -9,11 +8,7 @@ declare global {
 
 const config: LockliftConfig = {
   compiler: {
-    // Specify path to your TON-Solidity-Compiler
-    path: "/usr/local/bin/solc-e66e9ac",
-
-    // Or specify version of compiler
-
+    version: "0.62.0",
     // Specify config for extarnal contracts as in exapmple
     // This filed for generating types only
     externalContracts: {
@@ -21,10 +16,7 @@ const config: LockliftConfig = {
     }
   },
   linker: {
-    // Specify path to your stdlib
-    lib: "/Users/pavlovdog/TON-Solidity-Compiler/lib/stdlib_sol.tvm",
-    // // Specify path to your Linker
-    path: "/usr/local/bin/tvm_linker-0.16.1",
+    version: "0.15.48",
   },
   networks: {
     local: {
@@ -40,7 +32,6 @@ const config: LockliftConfig = {
       // This giver is the default local-node giverV2
       giver: {
         // Check if you need to provide a custom giver
-        giverFactory: (ever, keyPair, address) => new SimpleGiver(ever, keyPair, address),
         address: "0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415",
         key: "172af540e43a524763dd53b26a066d472a97c4de37d5498170564510608250c3",
       },
@@ -54,16 +45,13 @@ const config: LockliftConfig = {
         amount: 20,
       },
     },
-    mainnet: {
+    main: {
       // Specify connection settings for https://github.com/broxus/everscale-standalone-client/
       connection: "mainnetJrpc",
       // Here, default SafeMultisig wallet is used as a giver
       giver: {
-        giverFactory: (ever, keyPair, address) => new GiverWallet(ever, keyPair, address),
-        address: "0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415",
-        // you can use bip39 phrase instead of key
-        phrase: "action inject penalty envelope rabbit element slim tornado dinner pizza off blood",
-        accountId: 0,
+        address: "0:3bcef54ea5fe3e68ac31b17799cdea8b7cffd4da75b0b1a70b93a18b5c87f723",
+        key: process.env.MAIN_GIVER_KEY ?? "",
       },
       keys: {
         // Use everdev to generate your phrase
@@ -72,6 +60,24 @@ const config: LockliftConfig = {
         amount: 20,
       },
     },
+    venom: {
+      connection: {
+        id: 1000,
+        group: "group",
+        type: "jrpc",
+        data: {
+          endpoint: "https://jrpc-testnet.venom.foundation"
+        },
+      },
+      giver: {
+        address: process.env.VENOM_GIVER_ADDRESS ?? "",
+        phrase: process.env.VENOM_GIVER_PHRASE ?? "",
+        accountId: 0,
+      },
+      keys: {
+        amount: 20
+      }
+    }
   },
   // you can use any settings that mocha framework support
   mocha: {
---
+++ everscale/scripts/7-deploy-hidden-bridge.ts
@@ -0,0 +1,105 @@
+import {Address} from "locklift";
+import {logContract} from "../test/utils/logger";
+import {isValidTonAddress} from "../test/utils";
+const prompts = require("prompts");
+const ora = require("ora");
+
+
+const main = async () => {
+    const response = await prompts([
+        {
+            type: "text",
+            name: "owner",
+            message: "Contracts owner address",
+            validate: (value: any) =>
+                isValidTonAddress(value) ? true : "Invalid address",
+        },
+        {
+            type: "text",
+            name: "nativeProxy",
+            message: "Native proxy address",
+            validate: (value: any) =>
+                isValidTonAddress(value) ? true : "Invalid address",
+        },
+        {
+            type: "text",
+            name: "key",
+            message: "Backend public key",
+        }
+    ]);
+
+    const AlienTokenWalletPlatform = await locklift.factory.getContractArtifacts('AlienTokenWalletPlatform');
+    const signer = (await locklift.keystore.getSigner("0"))!;
+
+    const spinner = ora("Deploying mediator").start();
+
+    const {
+        contract: Mediator
+    } = await locklift.factory.deployContract({
+        contract: "Mediator",
+        constructorParams: {
+            _owner: new Address(response.owner),
+            _nativeProxy: new Address(response.nativeProxy),
+            _alienTokenWalletPlatformCode: AlienTokenWalletPlatform.code
+        },
+        initParams: {
+            _randomNonce: locklift.utils.getRandomNonce()
+        },
+        value: locklift.utils.toNano(2),
+        publicKey: signer.publicKey
+    });
+
+    spinner.stop();
+
+    await logContract('Mediator', Mediator.address);
+
+    spinner.start('Deploying event deployer');
+
+    const {
+        contract: EventDeployer
+    } = await locklift.factory.deployContract({
+        contract: 'EventDeployer',
+        constructorParams: {
+            _guardian: new Address(response.owner),
+            _owner: response.key
+        },
+        initParams: {
+            _randomNonce: locklift.utils.getRandomNonce()
+        },
+        value: locklift.utils.toNano(15),
+        publicKey: signer.publicKey
+    });
+
+    spinner.stop();
+
+    await logContract('EventDeployer', EventDeployer.address);
+
+    spinner.start('Deploying event closer');
+
+    const {
+        contract: EventCloser
+    } = await locklift.factory.deployContract({
+        contract: 'EventCloser',
+        constructorParams: {
+            _owner: response.key,
+            _deployer: EventDeployer.address
+        },
+        initParams: {
+            _randomNonce: locklift.utils.getRandomNonce()
+        },
+        value: locklift.utils.toNano(15),
+        publicKey: signer.publicKey
+    });
+
+    spinner.stop();
+
+    await logContract('EventCloser', EventCloser.address);
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((e) => {
+        console.log(e);
+        process.exit(1);
+    });
