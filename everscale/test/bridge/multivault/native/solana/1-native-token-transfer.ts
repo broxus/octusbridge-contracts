d3bf673bb9945bcedb6fe6c5db345930a1f8f97f
serejkaaa512 <5125402@mail.ru>
2023-10-09 14:12:23 +0300

    updated solana tests

--- everscale/test/bridge/multivault/native/solana/1-native-token-transfer.ts
+++ everscale/test/bridge/multivault/native/solana/1-native-token-transfer.ts
@@ -10,7 +10,7 @@ import {
   EverscaleSolanaEventConfigurationAbi,
   MultiVaultEverscaleEVMEventNativeAbi, MultiVaultEverscaleSolanaEventNativeAbi,
   MultiVaultEVMEverscaleEventNativeAbi,
-  ProxyMultiVaultNative_V3Abi,
+  ProxyMultiVaultNative_V3Abi, ProxyMultiVaultNative_V5Abi,
   SolanaEverscaleEventConfigurationAbi,
   StakingMockupAbi,
   TokenRootAbi,
@@ -38,7 +38,7 @@ let ethereumEverscaleEventConfiguration: Contract<EthereumEverscaleEventConfigur
 let everscaleEthereumEventConfiguration: Contract<EverscaleEthereumEventConfigurationAbi>;
 let solanaEverscaleEventConfiguration: Contract<SolanaEverscaleEventConfigurationAbi>;
 let everscaleSolanaEventConfiguration: Contract<EverscaleSolanaEventConfigurationAbi>;
-let proxy: Contract<ProxyMultiVaultNative_V3Abi>;
+let proxy: Contract<ProxyMultiVaultNative_V5Abi>;
 let initializer: Account;
 
 let tokenRoot: Contract<TokenRootAbi>;
