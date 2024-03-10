17f670c603e6dceb7ac1c095dae1b9624990b7b9
pavlovdog <pavlovdog.moscow@gmail.com>
2024-03-10 13:54:47 +0400

    Update configs
    - EVM msigs
    - Remove other networks than venom_main in locklift config

--- everscale/locklift.config.ts
+++ everscale/locklift.config.ts
@@ -74,40 +74,6 @@ const config: LockliftConfig = {
         amount: 20,
       },
     },
-    main: {
-      // Specify connection settings for https://github.com/broxus/everscale-standalone-client/
-      connection: "mainnetJrpc",
-      // Here, default SafeMultisig wallet is used as a giver
-      giver: {
-        address: "0:3bcef54ea5fe3e68ac31b17799cdea8b7cffd4da75b0b1a70b93a18b5c87f723",
-        key: process.env.MAIN_GIVER_KEY ?? "",
-      },
-      keys: {
-        // Use everdev to generate your phrase
-        // !!! Never commit it in your repos !!!
-        // phrase: "action inject penalty envelope rabbit element slim tornado dinner pizza off blood",
-        amount: 20,
-      },
-    },
-    venom_testnet: {
-      connection: {
-        id: 1000,
-        group: "group",
-        type: "jrpc",
-        data: {
-          endpoint: "https://jrpc-testnet.venom.foundation"
-        },
-      },
-      giver: {
-        address: process.env.VENOM_GIVER_ADDRESS ?? "",
-        phrase: process.env.VENOM_GIVER_PHRASE ?? "",
-        accountId: 0,
-      },
-      keys: {
-        phrase: process.env.VENOM_SEED ?? '',
-        amount: 20
-      }
-    },
     venom_main: {
       connection: {
         id: 1,
